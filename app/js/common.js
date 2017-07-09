2ac42e1c858f7d2920cb3dae5b3bf6b0ce6a6680
evm237 <evm237@gmail.com>
2017-07-09 22:45:48 +0300

    Header section

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
+    $("nav ul li a").click(function() {
+        $("nav ul li a").removeClass('active');
+        $(this).addClass('active');
+    })
+
+    $(".slider.owl-carousel").owlCarousel({
+        items : 1,
+        nav : true,
+        navText : " ",
+        loop : true,
+        autoplay : true,
+        autoplayHoverPause : true,
+        fluidSpeed : 600,
+        autoplaySpeed : 600,
+        navSpeed : 600,
+        dotsSpeed : 600,
+        dragEndSpeed : 600
+     });
+
 
 });
