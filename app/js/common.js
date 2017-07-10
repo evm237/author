e94f48a364473a5364098dbb5a7252dad9707245
evm237 <evm237@gmail.com>
2017-07-10 09:45:38 +0300

    Features for navigation(scroll, fixed) and button in main section

--- app/js/common.js
+++ app/js/common.js
@@ -1,8 +1,35 @@
 $(document).ready(function(){
-    $("nav ul li a").click(function() {
+    $("nav ul li a").click(function(event) {
+        event.preventDefault();
         $("nav ul li a").removeClass('active');
         $(this).addClass('active');
     })
+
+    $("header nav").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;  
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+    $(window).scroll(function() {
+        var top = $(document).scrollTop();
+        if (top > $('header').height()) $("nav").addClass("fixed");
+        else $("nav").removeClass("fixed");
+    });
+    $("nav").mouseover(
+        function(){
+            if( $(this).hasClass('fixed') ){
+                $(this).css({"opacity": 1});
+            }
+    });
+    $("nav").mouseout(
+        function(){
+            if( $(this).hasClass('fixed') ){
+                $(this).css({"opacity": 0.8});
+            }
+    });
+
 
     $(".slider.owl-carousel").owlCarousel({
         items : 1,
@@ -18,5 +45,13 @@ $(document).ready(function(){
         dragEndSpeed : 600
      });
 
+    $(".wrap_services button").mouseover(function(){
+        $(this).css({'background-color': "#49cbcd"});
+        $(this).parent().children('h4').css({'background-color': "#49cbcd"});
+    });
+    $(".wrap_services button").mouseout(function(){
+        $(this).css({'background-color': "#788492"});
+        $(".wrap_services h4").css({'background-color': "#485460"});
+    });
 
 });
